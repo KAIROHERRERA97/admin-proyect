cbd6e54482bbaec176385e88b11bdd339423071f
KAIROHERRERA97 <136655999+KAIROHERRERA97@users.noreply.github.com>
2024-03-05 23:44:05 -0500

    add: create folders

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+
+export default function AdminPage (){
+  return (
+    <div>
+    <h1> Estamos en la page de Admin </h1>
+    <Button> okis </Button>
+    </div>
+  )
+}
+
