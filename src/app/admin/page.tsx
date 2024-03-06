a1dc1e1140472744408b67f45f0497ebe5052517
KAIROHERRERA97 <136655999+KAIROHERRERA97@users.noreply.github.com>
2024-03-06 00:16:41 -0500

    add: component Header

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,12 +1,16 @@
 import { Button } from '@/components/ui/button'
+import Header from '@/components/ui/header'
 import React from 'react'
 
 export default function AdminPage (){
   return (
+    <main className='flex flex-col bg-slate-100 '>
+    <Header></Header>
     <div>
     <h1> Estamos en la page de Admin </h1>
     <Button> okis </Button>
     </div>
+    </main>
   )
 }
 
---
+++ src/components/ui/header.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+
+export default function Header() {
+  return (
+    <header className="w-full bg-white p-4 flex justify-between items-center">
+      <h1 className="text-xl font-semibold">Clínica Los Mata Sanos 2.0</h1>
+      <div className="flex justify-between  items-center ">
+        <span className="mr-4 text-md font-medium">Rick Melchor</span>
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </div>
+    </header>
+  );
+}
