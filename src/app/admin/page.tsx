060732148927ee3f392042ef8b6a856ec6c946e7
KAIROHERRERA97 <136655999+KAIROHERRERA97@users.noreply.github.com>
2024-03-06 22:42:23 -0500

    fix: sideBar

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -4,24 +4,22 @@ import SideBar from "@/components/ui/sideBar";
 
 export default function AdminPage() {
   return (
-    <>
-    <main className="flex flex-row ">
+    <main className="flex flex-row gap-1">
       <SideBar />
-      <section className="flex flex-col bg-slate-50 h-screen w-screen">
+      <section className="flex flex-col bg-slate-50 h-screen w-screen ">
         <Header />
         <div className="w-dvh pl-4">
           <div className="flex flex-row justify-between items-center p-2 ">
-            <span className="text-l font-semibold text-slate-400">
-              User Profile
+            <span className="text-l font-semibold text-slate-400 mr-6">
+               Profile
             </span>
-            <hr className="w-[55rem] " />
+            <hr className="w-full " />
           </div>
           <div className="h-96 bg-white m-4 mb-8 flex justify-between w-[60rem] items-center">
             holis bb
           </div>
         </div>
       </section>
-      </main>
-    </>
+    </main>
   );
 }
--- src/components/ui/sideBar.tsx
+++ src/components/ui/sideBar.tsx
@@ -3,30 +3,126 @@ import React from "react";
 export default function SideBar() {
   return (
     <>
-      <div className="bg-white h-screen w-60 p-4 flex flex-col gap-6 rounded-md	shadow-md ">
+      <div className="bg-white h-screen w-60 p-4 flex flex-col gap-6	shadow-xl ">
         <div className="flex justify-between items-center">
           <div className="font-mediun text-md">Admin</div>
-          <div>svg</div>
+          <div>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-menu-2"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#2c3e50"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M4 6l16 0" />
+              <path d="M4 12l16 0" />
+              <path d="M4 18l16 0" />
+            </svg>
+          </div>
         </div>
-        <span>Búsqueda</span>
+        <input
+          type="text"
+          placeholder="🔍Search"
+          className="w-full rounded-md border-2 border-input bg-background pr-2"
+        />
         <div className="flex flex-col">
           <span className="font-mediun text-md mb-2">Menú</span>
           <div className="flex items-center gap-2 p-1 rounded-md bg-slate-100">
-            <span>svg</span>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-home-2"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#94A3B8"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
+              <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
+              <path d="M10 12h4v4h-4z" />
+            </svg>
             <span className="text-sm text-slate-400 font-medium ">
               Dashboards
             </span>
           </div>
           <div className="flex items-center gap-2 p-1 rounded-md">
-            svg
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-user-cog"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#94A3B8"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" />
+              <path d="M6 21v-2a4 4 0 0 1 4 -4h2.5" />
+              <path d="M19.001 19m-2 0a2 2 0 1 0 4 0a2 2 0 1 0 -4 0" />
+              <path d="M19.001 15.5v1.5" />
+              <path d="M19.001 21v1.5" />
+              <path d="M22.032 17.25l-1.299 .75" />
+              <path d="M17.27 20l-1.3 .75" />
+              <path d="M15.97 17.25l1.3 .75" />
+              <path d="M20.733 20l1.3 .75" />
+            </svg>
             <span className="text-sm text-slate-400 font-medium ">Admins</span>
           </div>
           <div className="flex items-center gap-2 p-1 rounded-md">
-            svg
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-users"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#94A3B8"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M9 7m-4 0a4 4 0 1 0 8 0a4 4 0 1 0 -8 0" />
+              <path d="M3 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" />
+              <path d="M16 3.13a4 4 0 0 1 0 7.75" />
+              <path d="M21 21v-2a4 4 0 0 0 -3 -3.85" />
+            </svg>
             <span className="text-sm text-slate-400 font-medium ">Client</span>
           </div>
           <div className="flex items-center gap-2 p-1 rounded-md">
-            svg
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-credit-card-pay"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#94A3B8"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M12 19h-6a3 3 0 0 1 -3 -3v-8a3 3 0 0 1 3 -3h12a3 3 0 0 1 3 3v4.5" />
+              <path d="M3 10h18" />
+              <path d="M16 19h6" />
+              <path d="M19 16l3 3l-3 3" />
+              <path d="M7.005 15h.005" />
+              <path d="M11 15h2" />
+            </svg>
             <span className="text-sm text-slate-400 font-medium ">
               Suscriptions
             </span>
@@ -35,19 +131,67 @@ export default function SideBar() {
         <hr />
         <div className="flex flex-col">
           <div className="flex items-center gap-2 p-1 rounded-md">
-            svg
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-settings"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#94A3B8"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" />
+              <path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" />
+            </svg>
             <span className="text-sm text-slate-400 font-medium ">
               Settings
             </span>
           </div>
           <div className="flex items-center gap-2 p-1 rounded-md">
-            svg
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-license"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#94A3B8"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M15 21h-9a3 3 0 0 1 -3 -3v-1h10v2a2 2 0 0 0 4 0v-14a2 2 0 1 1 2 2h-2m2 -4h-11a3 3 0 0 0 -3 3v11" />
+              <path d="M9 7l4 0" />
+              <path d="M9 11l4 0" />
+            </svg>
             <span className="text-sm text-slate-400 font-medium ">
               Auditory
             </span>
           </div>
           <div className="flex items-center gap-2 p-1 rounded-md">
-            svg
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="icon icon-tabler icon-tabler-heart-handshake"
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="#94A3B8"
+              fill="none"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <path d="M19.5 12.572l-7.5 7.428l-7.5 -7.428a5 5 0 1 1 7.5 -6.566a5 5 0 1 1 7.5 6.572" />
+              <path d="M12 6l-3.293 3.293a1 1 0 0 0 0 1.414l.543 .543c.69 .69 1.81 .69 2.5 0l1 -1a3.182 3.182 0 0 1 4.5 0l2.25 2.25" />
+              <path d="M12.5 15.5l2 2" />
+              <path d="M15 13l2 2" />
+            </svg>
             <span className="text-sm text-slate-400 font-medium ">Support</span>
           </div>
         </div>
